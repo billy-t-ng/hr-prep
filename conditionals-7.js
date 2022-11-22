7e4cefe111265747b2d8db68b469288ed5b13846
Billy Ng <billyng4@gmail.com>
2022-11-21 17:38:09 -0800

    Add conditionals 7 exercises

---
+++ conditionals-7.js
@@ -0,0 +1,40 @@
+/* areValidCredentials
+
+
+Write a function called "areValidCredentials".
+
+Given a name and a password, "areValidCredentials", returns true if the name is longer than 3 characters, AND, the password is at least 8 characters long. Otherwise it returns false.
+ */
+
+function areValidCredentials(name, password) {
+    if (name.length > 3 && password.length >= 8){
+      return true;
+    }
+    return false;
+  }
+
+var output = areValidCredentials('Ritu', 'mylongpassword')
+console.log(output); // --> true
+
+/* findMinLengthOfThreeWords
+
+Write a function called "findMinLengthOfThreeWords".
+
+Given 3 words, "findMinLengthOfThreeWords" returns the length of the shortest word.
+
+ */
+
+function findMinLengthOfThreeWords(word1, word2, word3) {
+    let wordArr = [word1, word2, word3];
+    let shortestWord = word1;
+    for (let i = 1; i < wordArr.length; i++){
+      if (wordArr[i].length < shortestWord.length){
+        shortestWord = wordArr[i];
+        console.log(shortestWord)
+      }
+    }
+    return shortestWord.length;
+  }
+  
+var output = findMinLengthOfThreeWords('a', 'be', 'see');
+console.log(output); // --> 1
